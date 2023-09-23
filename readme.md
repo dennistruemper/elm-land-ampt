14ef7a2d9eee975e5bb3f5bf0b6844d4284e0c07
Dennis Trümper <privat@truemper-nord.de>
2023-09-23 20:24:51 +0200

    add documentation

---
+++ readme.md
@@ -0,0 +1,18 @@
+# elm-land and ampt
+
+example deployment: [https://solid-data-ush4cf.ampt.app/](https://solid-data-ush4cf.ampt.app/)
+
+## What are these?
+
+### [Elm Land](https://elm.land)
+
+Single Page Application Framework with [Elm](https://elm-lang.org/)
+
+### [ampt](https://getampt.com)
+
+A self provisioning runtime to have code runnig in AWS without managing infrastructure. Can host Websites too.
+
+## Why put these together?
+
+I like the ideas of Elm and the community a lot. Elm Land takes away a lot of the wiering and has awesome documentation. But I also need some backend. But I do not want to want to turn all the knobs the infrastructure provider has to offer. Also I really like the idea of serverless. Thats why I thought Elm Land and ampt are worth a try.
+This repository got created to showcase this combo. I can imagine a lot of developers don't know both of these.
--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -3,6 +3,7 @@ module Pages.Home_ exposing (Model, Msg, page)
 import Api.Counter exposing (Data, getNewCountFromMessage)
 import Effect exposing (Effect)
 import Html
+import Html.Attributes
 import Html.Events
 import Http
 import Page exposing (Page)
@@ -52,12 +53,12 @@ update : Shared.Model -> Msg -> Model -> ( Model, Effect Msg )
 update shared msg model =
     case msg of
         Increment ->
-            ( model
+            ( { model | countData = Api.Counter.Loading }
             , Effect.sendCmd <| Api.Counter.changeCounter { onResponse = GotNewCountResponse, counterId = "global", amount = 1, baseUrl = shared.baseUrl }
             )
 
         Decrement ->
-            ( model
+            ( { model | countData = Api.Counter.Loading }
             , Effect.sendCmd <| Api.Counter.changeCounter { onResponse = GotNewCountResponse, counterId = "global", amount = -1, baseUrl = shared.baseUrl }
             )
 
@@ -102,7 +103,8 @@ view model =
     { title = "Pages.Home_"
     , body =
         [ Html.div []
-            [ Html.div [] [ Html.button [ Html.Events.onClick Decrement ] [ Html.text "-" ] ]
+            [ Html.p [] [ Html.text "Keep this tab open and change value in an other Tab" ]
+            , Html.div [] [ Html.button [ Html.Events.onClick Decrement ] [ Html.text "-" ] ]
             , case model.countData of
                 Api.Counter.Loading ->
                     Html.div [] [ Html.text "Loading..." ]
@@ -111,9 +113,22 @@ view model =
                     Html.div [] [ Html.text "Error" ]
 
                 Api.Counter.Success count ->
-                    Html.div [] [ Html.text "Loaded" ]
-            , Html.div [] [ Html.text (String.fromInt model.count) ]
-            , Html.div [] [ Html.button [ Html.Events.onClick Increment ] [ Html.text "+" ] ]
+                    Html.div [] [ Html.text <| String.fromInt model.count ]
+            , Html.div []
+                [ Html.button [ Html.Events.onClick Increment ] [ Html.text "+" ] ]
+            , Html.p [] [ Html.text "This is an example project to show ", Html.a [ Html.Attributes.href "https://elm.land" ] [ Html.text "Elm-Land" ], Html.text " with ", Html.a [ Html.Attributes.href "https://getampt.com" ] [ Html.text "ampt" ], Html.text ". What may be of interest to you:" ]
+            , Html.ul []
+                [ Html.li [] [ Html.text "Ampt: express-js api - run ", Html.b [] [ Html.text "npx ampt" ], Html.text " to deploy your own sandbox" ]
+                , Html.li [] [ Html.text "Ampt: test with temporary backend - run ", Html.b [] [ Html.text "npm run dev-test" ], Html.text " to create a temporary environment, execute data layer tests and destroy environment after" ]
+                , Html.li [] [ Html.text "Ampt + Elm-Land: use websockets to notify other userers of change. Open multiple browsers / tabs, change the counter and see the change in other browsers/tabs. This feature is a separate commit so you can see all the chane in elm-land and ampt that is necessary" ]
+                , Html.li [] [ Html.text "Ampt + Elm-Land: easy deployment I. Frontend and backend are at the same base URL, so you can call backend api with just the routes. Deployment does not need to know the URL of backend. Just needs to be inserted at local dev like this: ", Html.b [] [ Html.text "BASE_URL=https://your-nice.ampt.app npx elm-land server" ] ]
+                , Html.li [] [ Html.text "Ampt + Elm-Land: easy deployment II. Build elm-land an copy output to /static directory. Run ampt deploy. Your project is live. ", Html.b [] [ Html.text "npm run deploy test" ], Html.text " You can name the stage other than test, maybe prod some day" ]
+                , Html.br [] []
+                , Html.br [] []
+                , Html.br [] []
+                , Html.br [] []
+                , Html.p [ Html.Attributes.style "color" "red" ] [ Html.text "Just a DEMO, do not use theese settings for production." ]
+                ]
             ]
         ]
     }
